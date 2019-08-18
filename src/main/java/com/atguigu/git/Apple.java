9aacd575083a3cd198e951ab61ba752ed887e6ed
lzh <luzehua@zhongbei.com>
2019-08-18 16:55:41 +0800

    Apple Git3

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -3,6 +3,6 @@ package com.atguigu.git;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("I am a apple");
-		System.out.println("哈哈哈哈,我又来了");
+		System.out.println("哈哈哈哈,我又来");
 	}
 }
